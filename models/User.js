224e38a7189c56c2efaaabde6fd702d3fc08f78b
Abdullah Ansari <aa9272137@gmail.com>
2020-08-25 00:41:52 +0500

    completed validation and cleanUp()

--- models/User.js
+++ models/User.js
@@ -1,19 +1,37 @@
 //here in model part we will actually model data we recieve controller requires it from here.
 //this points to whoever is calling here user is calling the register() function
 //You can't use arrow functions for defining a constructor function.
+const validator = require('validator');
 let User = function(data) {
     this.data = data;
     this.errors = [];
 }
+User.prototype.cleanUp = function() {
+    //here ifuser input bogus data we deal with it
+    //just extra not necessarily needed
+    if(typeof(this.data.username) != 'string'){this.data.username = ''};
+    if(typeof(this.data.email) != 'string'){this.data.email = ''};
+    if(typeof(this.data.password) != 'string'){this.data.password = ''};
+
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
+}
+
 User.prototype.validate = function() {
     if (this.data.username == ''){this.errors.push('You cannot leave username empty')};
-    if (this.data.email == ''){this.errors.push('You cannot leave email empty')};
+    if (this.data.username != '' && !validator.isAlphanumeric(this.data.username)){this.errors.push('username can only contain letters or alphabets')};
+    if (!validator.isEmail(this.data.email)){this.errors.push('Please ener a valid email address')};
     if (this.data.password == ''){this.errors.push('You cannot leave password empty')};
-
+    if (this.data.username.length > 0 && this.data.username.length < 3){this.errors.push('username must be atleast 3 characters long')};
+    if (this.data.password.length > 0 && this.data.password.length < 12){this.errors.push('password must be atleast 12 characters long')};
 }
 User.prototype.register = function() {
     //Validate user data
-    this.validate()
+    this.cleanUp();
+    this.validate();
 
 
     //If there are no validation error than
